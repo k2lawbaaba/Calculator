dd2d6b911403ced146a887abf1239750c0df0ea8
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-05-16 14:02:27 +0100

    calculator

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const symbols=document.getElementsByClassName('symbol')
+const specialOps=document.getElementsByClassName('specialOps')
 const buttons = document.getElementsByClassName('figSym')
 const displayTray= document.getElementById('display');
+const operator=document.getElementsByClassName('operator');
+var equaltoClicked = false;
+let oper=['*','+','/','.','-']
 
+
+// to iterate the buttons 
 for(let button of buttons){
     button.addEventListener('click', () => {
         switch (button.innerText) {
@@ -12,40 +17,62 @@ for(let button of buttons){
                  displayTray.value += button.innerText;
                 break;
             case "1":
-                displayTray.value += button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
                 break;
             case "2":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
                 break; 
             case "3":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
                 break; 
             case "(":
                 displayTray.value +=button.innerText;
                 break; 
             case "4":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
                 break; 
             case "5":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
                 break; 
             case "6":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
                 break;
               case "7":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
              break;
              case "8":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
              break;
              case "9":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
              break;
              case "0":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
              break;
              case "00":
-                displayTray.value +=button.innerText;
+                if(equaltoClicked == false) displayTray.value += button.innerText;
+
+                else reset(button.innerText);
              break;
             default:
             displayTray +=button.innerText;
@@ -55,22 +82,71 @@ for(let button of buttons){
 }
 
 
-function factorial(screen){
+function factorial(input){
     var num, f;
     f=1
-    num = screen.value;
+    num = input.value;
     for(var i=1; i<=num; i++){
         f = f*i;
     }
     i=i-1;
-    screen.value=f;
+    input.value=f;
 }
 
+// iterating the special buttons 
+    for(let spacialsButton of specialOps){
+        $(spacialsButton).click(()=>{
+            switch (spacialsButton.innerText) {
+                case 'cos':
+                    displayTray.value = eval(Math.cos(displayTray.value).toFixed(4));
+                    break;
+                case 'sin':
+                    displayTray.value = eval(Math.sin(displayTray.value).toFixed(4));
+                    break;
+                case 'tan':
+                    displayTray.value = eval(Math.tan(displayTray.value).toFixed(4));
+                    break;
+                case 'log':
+                    displayTray.value = eval(Math.log(displayTray.value).toFixed(4));
+                    break;
+                case '√':
+                    displayTray.value = eval(Math.sqrt(displayTray.value).toFixed(4));
+                    break;
+                case '%':
+                    displayTray.value = eval(displayTray.value / 100);
+                    break;
+                case 'C':
+                    displayTray.value = displayTray.value.toString().slice(0,-1);
+                    break;
+                case 'π':
+                    displayTray.value = eval((Math.PI * displayTray.value).toFixed(4));
+                    break;
+                case 'x!':
+                    displayTray.value = factorial(displayTray.value);
+                    break;
+                case 'xy':
+                    displayTray.value = eval(Math.pow(displayTray.value,2).toFixed(4));
+                    break;
+                case '=':
+                        displayTray.value = eval(displayTray.value);
+                        equaltoClicked =true;                   
+                    break;
+                default:
+                    break;
+            }
+        })
+    }
 
+//  to iterate over the array objects of  operators buttons
+       for(let opps of operator){
+            $(opps).click(()=>{
+                operators(opps,displayTray);
+            })
+       }
 
 function operators(element, dis){
-    let elem= document.getElementById(element);
-    let attr= elem.getAttribute('value');
+    // let elem= document.getElementById(element);
+    let attr= element.getAttribute('value');
     let displayValue= dis.value
 
      
@@ -98,4 +174,10 @@ function operators(element, dis){
     else {
         dis.value += attr;
     }
+}
+
+function reset(input){
+    let displayValue = displayTray.value;
+    (oper.includes(displayValue[displayValue.length-1]))?displayTray.value += input : displayTray.value = input;
+    equaltoClicked = false;
 }
